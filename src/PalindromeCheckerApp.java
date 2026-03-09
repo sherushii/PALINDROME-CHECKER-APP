f1815483a9691041a2a9ce5ecfb21dc7804d8e5f
sherushii <ac1627@srmist.edu.in>
2026-03-09 14:59:36 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,10 @@
 public class PalindromeCheckerApp {
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Name: Palindrome Checker");
+        System.out.println("Version: 1.0");
+
+    }
+
